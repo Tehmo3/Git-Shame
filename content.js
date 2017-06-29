39ec6e0d6a647d40e79f8326fe8aa6642903536e
Tehmo3 <jasper.b.miles@gmail.com>
2017-06-29 18:35:35 +1000

    stuff

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("Hey!");
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.github.com/users/Tehmo3/events", true);
+xhr.onload = function (e) {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.error(xhr.statusText);
+    }
+  }
+};
+xhr.onerror = function (e) {
+  console.error(xhr.statusText);
+};
+xhr.send();
